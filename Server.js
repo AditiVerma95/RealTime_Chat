cbd50d5398731a9c674fe7f44c6f19006da0e508
Aditi Verma <anvil557@gmail.com>
2025-11-11 21:54:53 +0530

    server file changes

--- Server.js
+++ Server.js
@@ -21,7 +21,7 @@ io.on("connection", (socket) => {
     });
 });
 
-// Start server on port 3000
-http.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
